51c751df0dfbcd6df363be0f64c65e8329ec4c0f
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-12-08 15:03:31 +0530

    update project

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Typewriter } from "react-simple-typewriter";
+import { Heart } from "lucide-react";
+import { useEffect, useState } from "react";
+
 function Home() {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const [isLoved, setIsLoved] = useState(false);
+  const [loveCount,setLoveCount]=useState(0);
+  useEffect(()=>{
+    fetch(import.meta.env.VITE_BACKEND_URL+"",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"}
+    })
+    .then((res)=>res.json())
+    .then((res)=>{
+
+    })
+    .catch((err)=>console.log(err))
+  },[]);
+
   return (
     <div
       style={{
@@ -29,8 +46,8 @@ function Home() {
           <div className="">
             <p>I am&nbsp;</p>
             <Typewriter
-              words={["Ayantik Sarkar","A passionate Web Developer"]}
-              loop={0} 
+              words={["Ayantik Sarkar", "A passionate Web Developer"]}
+              loop={0}
               cursor
               cursorStyle="|"
               typeSpeed={80}
@@ -41,14 +58,44 @@ function Home() {
         </div>
         <div className="flex gap-5 mt-[5rem]" id="intro-buttons">
           <button className="bg-linear-65 from-purple-500 to-pink-500 px-10 py-1 rounded-[2rem] text-[1.5rem] hover:scale-[1.05] duration-200 font-semibold">
-            <a href="https://drive.google.com/file/d/1TmuYrMKZNFIZGZwdF0aLSo1ypNNkmlCT/view?usp=sharing" target="_blank">Get Résumé</a>
+            <a
+              href="https://drive.google.com/file/d/1TmuYrMKZNFIZGZwdF0aLSo1ypNNkmlCT/view?usp=sharing"
+              target="_blank"
+            >
+              Get Résumé
+            </a>
           </button>
-          <button className="px-8 py-1 rounded-[2rem] text-[1.5rem] hover:scale-[1.05] duration-200 border-pink-500 border-2 text-white font-semibold" onClick={()=>{
-            navigate("/contact");
-          }}>
+          <button
+            className="px-8 py-1 rounded-[2rem] text-[1.5rem] hover:scale-[1.05] duration-200 border-pink-500 border-2 text-white font-semibold"
+            onClick={() => {
+              navigate("/contact");
+            }}
+          >
             Contact Me!
           </button>
         </div>
+
+        {/* <div className="flex gap-4 mt-10 items-center">
+          <div className="text-[2rem] font-bold bg-gradient-to-r from-pink-400 via-orange-500 to-red-600 bg-clip-text text-transparent animate-gradient">
+            Give a Heart 👉
+          </div>
+          <Heart
+            className="text-[5rem] stroke-[#ff0000] mt-[0.3rem] hover:scale-[1.16] duration-300"
+            size={40}
+            onClick={(e) => {
+              if (!isLoved) {
+                e.currentTarget.style.fill = "red";
+                setIsLoved(true);
+                setLoveCount(loveCount+1);
+              } else {
+                e.currentTarget.style.fill = "transparent";
+                setIsLoved(false);
+                setLoveCount(loveCount-1);
+              }
+            }}
+          />
+          <p className="text-gray-400 text-[2rem]">{loveCount}</p>
+        </div> */}
       </div>
     </div>
   );
--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import simonSays from "../assets/simonSays.png";
 import todoList from "../assets/todolist.png";
 import nitrConfessions from "../assets/nitrConfessions.png";
-import elimics from "../assets/elimics.png"
+import codehive from "../assets/codehive.png";
 
 function Projects() {
   return (
@@ -28,68 +28,138 @@ function Projects() {
         <div className="flex flex-col mt-[4rem] gap-5 mb-5">
           <div className="flex gap-[2rem] mx-3 flex-col">
             <div>
-              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">1) Simon Says Game</h1>
+              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">
+                1) CodeHive (Work in Progress)
+              </h1>
+              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
+                This is a real-time collaborative code editor designed to make
+                coding, sharing, and learning smoother for developers.
+              </h2>
+            </div>
+            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
+              <img src={codehive} alt="" className="w-[40rem] rounded-[1rem]" />
+              <div className="flex mt-2 gap-4">
+                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://github.com/ayantik2006/CodeHive"
+                    target="_blank"
+                  >
+                    <i className="fa-brands fa-github mr-1"></i>GitHub
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-[2rem] mx-3 flex-col">
+            <div>
+              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">
+                2) NITR Confessions
+              </h1>
+              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
+                A fun website for the students of NIT Rourkela. Here the
+                students can log in anonymously and share anything without
+                hesitation. Other students can react and comments on them too.
+              </h2>
+            </div>
+            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
+              <img
+                src={nitrConfessions}
+                alt=""
+                className="w-[40rem] rounded-[1rem]"
+              />
+              <div className="flex mt-2 gap-4">
+                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300">
+                  <a href="https://nitrconfessions.fun/" target="_blank">
+                    <i className="fa-solid fa-up-right-from-square mr-1"></i>
+                    Visit
+                  </a>
+                </button>
+                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://github.com/ayantik2006/NITR-Confessions"
+                    target="_blank"
+                  >
+                    <i className="fa-brands fa-github mr-1"></i>GitHub
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-[2rem] mx-3 flex-col">
+            <div>
+              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">
+                3) CommitList - A todo list
+              </h1>
+              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
+                Users can create account and log in to their respective
+                accounts, then they can create todo tasks, if needed they can
+                edit them too(shows the date and time of last edit). They can
+                mark the task as "done" and delete it if not needed
+              </h2>
+            </div>
+            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
+              <img src={todoList} alt="" className="w-[40rem] rounded-[1rem]" />
+              <div className="flex mt-2 gap-4">
+                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://commitlist-a-todo-list-project.onrender.com/"
+                    target="_blank"
+                  >
+                    <i className="fa-solid fa-up-right-from-square mr-1"></i>
+                    Visit
+                  </a>
+                </button>
+                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://github.com/ayantik2006/CommitList-A-todo-list-project"
+                    target="_blank"
+                  >
+                    <i className="fa-brands fa-github mr-1"></i>GitHub
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-[2rem] mx-3 flex-col ">
+            <div>
+              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">
+                4) Simon Says Game
+              </h1>
               <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
                 A simple game of Simon Says made using pure HTML, CSS and
                 JavaScript.
               </h2>
             </div>
             <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
-              <img src={simonSays} alt="" className="w-[40rem] rounded-[1rem]" />
+              <img
+                src={simonSays}
+                alt=""
+                className="w-[40rem] rounded-[1rem]"
+              />
               <div className="flex mt-2 gap-4">
-                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300"><a href="https://simon-says-game-basic-qrr71xxnd-ayantik-sarkars-projects.vercel.app/" target="_blank"><i className="fa-solid fa-up-right-from-square mr-1"></i>Visit</a></button>
-                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300"><a href="https://github.com/ayantik2006/simon-says-game-basic" target="_blank"><i className="fa-brands fa-github mr-1"></i>GitHub</a></button>
+                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://simon-says-game-basic-qrr71xxnd-ayantik-sarkars-projects.vercel.app/"
+                    target="_blank"
+                  >
+                    <i className="fa-solid fa-up-right-from-square mr-1"></i>
+                    Visit
+                  </a>
+                </button>
+                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300">
+                  <a
+                    href="https://github.com/ayantik2006/simon-says-game-basic"
+                    target="_blank"
+                  >
+                    <i className="fa-brands fa-github mr-1"></i>GitHub
+                  </a>
+                </button>
               </div>
             </div>
           </div>
-
-          <div className="flex gap-[2rem] mx-3 flex-col">
-            <div>
-              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">2) CommitList - A todo list</h1>
-              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
-                Users can create account and log in to their respective accounts, then they can create todo tasks, if needed they can edit them too(shows the date and time of last edit). They can mark the task as "done" and delete it if not needed
-              </h2>
-            </div>
-            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
-              <img src={todoList} alt="" className="w-[40rem] rounded-[1rem]" />
-              <div className="flex mt-2 gap-4">
-                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300"><a href="https://commitlist-a-todo-list-project.onrender.com/" target="_blank"><i className="fa-solid fa-up-right-from-square mr-1"></i>Visit</a></button>
-                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300"><a href="https://github.com/ayantik2006/CommitList-A-todo-list-project" target="_blank"><i className="fa-brands fa-github mr-1"></i>GitHub</a></button>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex gap-[2rem] mx-3 flex-col">
-            <div>
-              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">3) NITR Confessions</h1>
-              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
-                A fun website for the students of NIT Rourkela. Here the students can log in anonymously and share anything without hesitation. Other students can react and comments on them too.
-              </h2>
-            </div>
-            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
-              <img src={nitrConfessions} alt="" className="w-[40rem] rounded-[1rem]" />
-              <div className="flex mt-2 gap-4">
-                <button className="bg-pink-700 px-3 py-1 rounded-[2rem] font-semibold hover:scale-[1.05] duration-300"><a href="https://nitrconfessions.fun/" target="_blank"><i className="fa-solid fa-up-right-from-square mr-1"></i>Visit</a></button>
-                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300"><a href="https://github.com/ayantik2006/NITR-Confessions" target="_blank"><i className="fa-brands fa-github mr-1"></i>GitHub</a></button>
-              </div>
-            </div>
-          </div>  
-
-          <div className="flex gap-[2rem] mx-3 flex-col">
-            <div>
-              <h1 className="text-[4rem] font-bold bg-gradient-to-r from-green-400 via-pink-500 to-purple-600 bg-clip-text text-transparent animate-gradient project-heading">4) Elimics</h1>
-              <h2 className="text-[1.3rem] max-w-[30rem] text-gray-400 italic font-semibold">
-                This is a platform that I am collboratively developing along with Shuvam Sathapathi and Arka Pal. For now I am the solo developer and it's still under development.
-              </h2>
-            </div>
-            <div className="p-2 bg-purple-800 rounded-[1rem] flex flex-col items-center border-2 border-pink-700">
-              <img src={elimics} alt="" className="w-[40rem] rounded-[1rem]" />
-              <div className="flex mt-2 gap-4">
-                <button className="text-white font-semibold bg-black px-2 py-1 rounded-[2rem] hover:scale-[1.05] duration-300"><a href="https://github.com/ayantik2006/elimics" target="_blank"><i className="fa-brands fa-github mr-1"></i>GitHub</a></button>
-              </div>
-            </div>
-          </div> 
-
         </div>
       </div>
     </div>
